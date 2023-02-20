3264585008951a1cc317c962ddd16cd6df142de5
peternguyen-iq <122076882+peternguyen-iq@users.noreply.github.com>
2023-02-20 13:42:39 +0800

    Update IdentityContextModelSnapshot.cs

--- Migrations/Identity/IdentityContextModelSnapshot.cs
+++ Migrations/Identity/IdentityContextModelSnapshot.cs
@@ -13,7 +13,7 @@ namespace ProDat.Web2.Migrations.Identity
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 6181
+#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "3.1.7")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
